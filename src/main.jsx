4d48508860846a90ff6a33fc7f47a3ff3c2f7c96
zeru-program <justinebogor0609@gmail.com>
2024-12-17 07:45:33 +0700

    push from termux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import RouterApp from './be/Router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterApp />
   </StrictMode>,
 )
